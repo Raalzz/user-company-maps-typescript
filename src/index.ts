955c4967aa61f313082e0833a90df543e800239d
Rahul Sawant <rahulsawanth06@gmail.com>
2019-08-24 14:08:03 +0530

    Showing map on the Screen

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
-
-google;
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: { lat: 0, lng: 0 }
+});
