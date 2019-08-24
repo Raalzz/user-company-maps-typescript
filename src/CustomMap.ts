0f8e6db993af27f927261bc7cff2118426ac99ee
Rahul Sawant <rahulsawanth06@gmail.com>
2019-08-24 14:45:07 +0530

    Implimented markers to user and the company

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -5,6 +8,25 @@ export class CustomMap {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: { lat: 0, lng: 0 }
+    });
+  }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng
+      }
+    });
+  }
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng
+      }
     });
   }
 }
