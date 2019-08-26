74c3f648ce1d00e31e0527c43ee9c157425eef9f
Rahul Sawant <rahulsawanth06@gmail.com>
2019-08-26 13:33:26 +0530

    Refactoring and removing Dead Codes

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,14 @@
 import { User } from "./User";
 import { Company } from "./Company";
 
+// Instructions to every other class
+// on how they can be an argument to "addMarker"
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -11,21 +19,12 @@ export class CustomMap {
     });
   }
 
-  addUserMarker(user: User): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng
-      }
-    });
-  }
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
       }
     });
   }
